7d6e17fde9e559a08ce19c4da01e3a722b2b6f2e
Gregor Pfeifer <gregor.pfeifer.gp@gmail.com>
2022-06-18 12:42:28 +0200

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Button from "./Button.svelte";
+  import ButtonBlocking from "./ButtonBlocking.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Hello Event Loop</h1>
+	<h2>It is single threaded!</h2>
+	<Button />
+  <p/>
+  <ButtonBlocking />
+  
+
+</main>
---
+++ ButtonBlocking.svelte
@@ -0,0 +1,54 @@
+<script>
+  let result = "Ready";
+  function calculatePrimes(iterations, multiplier) {
+    var primes = [];
+    for (var i = 0; i < iterations; i++) {
+      var candidate = i * (multiplier * Math.random());
+      var isPrime = true;
+      for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+        if (candidate % c === 0) {
+          // not prime
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        primes.push(candidate);
+      }
+    }
+    return primes;
+  }
+
+  function factorial(n) {
+    if (n <= 1) {
+      return 1;
+      //recursive case
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  function long(n) {
+    while (n > 0) {
+      n--;
+    }
+  }
+
+  function handleClick() {
+    long(10000000000);
+    result = "Done";
+  }
+</script>
+
+<style>
+	button {
+	  background: #ff3e00;
+	  color: white;
+	  border: none;
+	  padding: 8px 12px;
+	  border-radius: 2px;
+	}
+</style>
+
+<button on:click={handleClick}>
+  State: {result} 
+</button>
